471ef6d13562b6691c67e1c689b9c51bf93ebae9
Mariusz Flis <mariusz.flis@gmail.com>
2019-06-10 19:30:44 +0200

    feat: First JavaScript

---
+++ main.js
@@ -0,0 +1,9 @@
+alert('Witaj na mojej stronie');
+console.log('Witaj na mojej stronie');
+const name='Mariusz';
+const age=39;
+/*console.log(name);
+console.log(age);*/
+console.log(`Nazywam się ${name} mam ${age} lat, a to jest moj pierwszy skrypt w JavaScript.`)
+
+
