bad18b01971e3f264986538b5c0a38dcbacbc3a8
Mariusz Flis <mariusz.flis@gmail.com>
2019-08-09 12:45:52 +0200

    BEM fix final?, Introduction to DOM ,JS added

--- main.js
+++ main.js
@@ -4,6 +4,16 @@ const name='Mariusz';
 const age=39;
 /*console.log(name);
 console.log(age);*/
-console.log(`Nazywam się ${name} mam ${age} lat, 🙈🙉🙊 to moja pierwsza strona WWW, i skrypt w JS 🛸`)
+console.log(`Mam na imię ${name} mam ${age} lat, 🙈🙉🙊 to moja pierwsza strona WWW, i skrypt w JS 🛸`)
+
+const about = document.querySelector('.about__paragraph--js');
+console.log(about);
+console.log(about.innerHTML);
+
+about.innerHTML = `${about.innerHTML} : Mam na imię ${name} mam ${age} lat, 🙈🙉🙊 to moja pierwsza strona WWW, i skrypt w JS 🛸 <br> Początki z <b>D</b>ocument <b>O</b>bject <b>M</b>odel`;
+
+console.log(about.innerHTML);
+
+
 
 
